d4040b0e8b8ca68a6b741b0e7d6868fdbd3ba603
Tor Home PC <tor.aksel.shepherd@gmail.com>
2021-02-18 16:05:15 -0500

    added shovels, axes, hoes, picks

--- src/inspect_sword_animation.py
+++ src/inspect_sword_animation.py
@@ -8,3 +8,19 @@ create_animation_from_timeline('sword_2', model_parent='handheld')
 
 for item in ['wooden_sword', 'stone_sword', 'iron_sword', 'golden_sword', 'diamond_sword', 'netherite_sword']:
     write_model(item, animation_name='sword_2')
+
+
+for item in ['wooden_pickaxe', 'stone_pickaxe', 'iron_pickaxe', 'golden_pickaxe', 'diamond_pickaxe', 'netherite_pickaxe']:
+    write_model(item, animation_name='sword_2')
+
+
+for item in ['wooden_axe', 'stone_axe', 'iron_axe', 'golden_axe', 'diamond_axe', 'netherite_axe']:
+    write_model(item, animation_name='sword_2')
+
+
+for item in ['wooden_shovel', 'stone_shovel', 'iron_shovel', 'golden_shovel', 'diamond_shovel', 'netherite_shovel']:
+    write_model(item, animation_name='sword_2')
+
+
+for item in ['wooden_hoe', 'stone_hoe', 'iron_hoe', 'golden_hoe', 'diamond_hoe', 'netherite_hoe']:
+    write_model(item, animation_name='sword_2')
